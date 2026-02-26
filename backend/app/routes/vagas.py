8be335c787bb51772816672bd9d4cff3947987ff
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-02-25 22:54:55 -0300

    feat: rota de candidato por vaga no back

--- backend/app/routes/vagas.py
+++ backend/app/routes/vagas.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 
 from app.core.database import get_session
 from app.core.security import get_usuario_admin
@@ -55,3 +55,34 @@ async def listar_vagas(session: Session = Depends(get_session)):
         })
 
     return vagas_formatadas
+
+@vagas_router.get("/{vaga_id}/candidatos")
+async def listar_candidatos_vaga(vaga_id: int, session: Session = Depends(get_session)):
+
+    vaga = session.query(Vaga).filter(Vaga.id == vaga_id).first()
+
+    if not vaga:
+        raise HTTPException(status_code=404, detail="Vaga não encontrada")
+    
+    vaga_formatada = {
+        "cargo": vaga.cargo,
+        "tipo": vaga.tipo,
+        "local": vaga.local, 
+        "status": vaga.status
+    }
+
+    candidatos = []
+
+    for candidatura in vaga.candidaturas:
+        candidatos.append({
+            "id": candidatura.candidato.id,
+            "nome": candidatura.candidato.usuario.nome,
+            "estado": candidatura.candidato.usuario.estado,
+            "status": candidatura.status,
+            "afinidade": candidatura.afinidade
+        })
+
+    return {
+        "vaga": vaga_formatada,
+        "candidatos": candidatos
+    }
