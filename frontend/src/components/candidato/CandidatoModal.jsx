df2ea512c4fd32a49360c9bc0ce4c83e50f04199
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-02-27 19:29:39 -0300

    feat: estrutura do componente de candidatos

---
+++ frontend/src/components/candidato/CandidatoModal.jsx
@@ -0,0 +1,29 @@
+import "./CandidatoModal.css"
+
+export const CandidatoModal = () => {
+    return (
+        <div id="candiato-main">
+            <h1>Gerenciar Candidatos</h1>
+            <div>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
