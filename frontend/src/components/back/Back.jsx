eda081ea04d939a33f65dcf3ae87e0e19b1a675e
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-03-06 20:57:45 -0300

    fix: troca de imagem por icone bootstrap na seta de voltar

--- frontend/src/components/back/Back.jsx
+++ frontend/src/components/back/Back.jsx
@@ -1,15 +1,13 @@
 import "./Back.css"
-import back_arrow from "../../assets/back.png"
 
 export const Back = ({ onClick }) => {
     return(
         <div id="component-back">
 
             <button id="component-back-button" onClick={onClick}>
-                <img id="component-back-img" src={back_arrow} alt="voltar.png"/>
+                <i class="bi bi-arrow-left"></i>
                 <p id="component-back-text">Voltar</p>
             </button>
-            
         </div>
     )
 }
