b7a6160cbcc188ec6b18e8a3b0736fb2fe188651
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-03-23 18:53:06 -0300

    feat: novo posicionamento de botões uteis

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,10 +1,23 @@
 import "./Header.css"
 import logo from "../../assets/bb_logo.svg"
 
+import { ThemeToggle } from "../theme/ThemeToggle"
+import { LogoutButton } from "../logout/LogoutButton"
+import { PerfilButton } from "../perfil/PerfilButton"
+
 export const Header = () => {
     return(
         <header id="component-header">
-            <img id="component-header-img" src={logo} alt="logo" />
+
+            <div id="component-header-left">
+                <img id="component-header-img" src={logo} alt="logo" />
+            </div>
+
+            <nav id="component-header-right">    
+                <ThemeToggle />
+                <PerfilButton />
+                <LogoutButton />
+            </nav>
         </header>
     )
 }
---
+++ frontend/src/components/perfil/PerfilButton.jsx
@@ -0,0 +1,12 @@
+import "./PerfilButton.css";
+
+export const PerfilButton = () => {
+    return (
+        <div id="perfil-container">
+            <button id="perfil-button">
+                <i class="bi bi-person-circle"></i>
+                <p id="perfil-text">Perfil</p>
+            </button>
+        </div>
+    )
+}
